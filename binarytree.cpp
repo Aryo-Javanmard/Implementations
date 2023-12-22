a80973f5c502fd33be11dc39f98d823d6c857a24
Aryo <88022310+Aryo-Javanmard@users.noreply.github.com>
2023-12-23 00:27:30 +1030

    Create binarytree.cpp

---
+++ binarytree.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class TreeNode {
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+class BinaryTree {
+public:
+    TreeNode* root;
+
+    BinaryTree() : root(nullptr) {}
+
+
+};
