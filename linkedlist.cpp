f2418e3b865a2034216518c5d99dc8587ff44151
Aryo <88022310+Aryo-Javanmard@users.noreply.github.com>
2023-12-23 00:20:07 +1030

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() : head(nullptr) {}
+
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
